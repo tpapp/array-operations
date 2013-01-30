be1c6e9c382252f573f2459463ec49378b5b7a5d
tpapp <tkpapp@gmail.com>
2013-01-30 15:51:51 +0100

    Added default method for as-array.

--- src/general.lisp
+++ src/general.lisp
@@ -49,4 +49,6 @@
 (defgeneric as-array (object)
   (:documentation "Return the contents of OBJECT as an array.  Exact semantics depends on OBJECT, but generally objects which contain elements in a rectilinear coordinate system should have a natural mapping to arrays.
 
-When the second value is T, the array itself does not share structure with OBJECT, but its elements may.  Otherwise, it is indeterminate whether the two objects share structure, and consequences of modifying the result are not defined.  Methods are encouraged but not required to return a second value."))
+When the second value is T, the array itself does not share structure with OBJECT, but its elements may.  Otherwise, it is indeterminate whether the two objects share structure, and consequences of modifying the result are not defined.  Methods are encouraged but not required to return a second value.")
+  (:method ((array array))
+    array))
